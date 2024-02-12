f895f7c53dc61e22c6147048ea83e026b7cdf687
Phuong Nguyen <phuongnguyen@MBP-de-Phuong.lan>
2024-02-12 18:21:41 +0100

    a

--- app/assets/images.js
+++ app/assets/images.js
@@ -1,5 +1,11 @@
 import slide1 from '../../public/slide1.png'
 import slide2 from '../../public/slide2.png'
+import logo from '../../public/react.png'
+import logo1 from '../../public/Js.png'
+import logo2 from '../../public/Angular.png'
+import logo3 from '../../public/tailwind.png'
+import logo4 from '../../public/mongodb.png'
+import logo5 from '../../public/nodejs.png'
 
 
-export default [slide1,slide2]
+export default [slide1,slide2,logo,logo1,logo2,logo3,logo4,logo5]
--- app/components/Slideshow.jsx
+++ app/components/Slideshow.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import slide1 from '../../public/slide1.png'
 import slide2 from '../../public/slide2.png'
 import logo from '../../public/react.png'
-import logo1 from '../../public/js.png'
+import logo1 from '../../public/Js.png'
 import logo2 from '../../public/Angular.png'
 import logo3 from '../../public/tailwind.png'
 import logo4 from '../../public/mongodb.png'
